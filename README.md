aa6b863ba049fcc8e91c3810f51a5cfef829a1b4
Jiseok CHOI <jiseok.dev@gmail.com>
2022-05-15 23:46:50 +0900

    Write main, Add Output

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 ## Together with the Rust async book
 
 <https://rust-lang.github.io/async-book/02_execution/01_chapter.html>
+
+## Output
+
+```shell
+Spawner: Sending new future(s) as task(s)...
+Spawner: Start!
+Executor: Polling TimerFuture...
+Executor: Timer still going... I should come back later
+Future: Timer completed
+Waker: Hey Executor, wake up! I just sent a task
+Executor: Polling TimerFuture...
+Spawner: Finish!
+```
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
+use std::time::Duration;
+
+use timer_future::TimerFuture;
+
 mod executor;
 mod timer_future;
 
 fn main() {
-    println!("Hello, world!");
+    let (executor, spawner) = executor::new_executor_and_spawner();
+
+    spawner.spawn(async {
+        println!("Spawner: Start!");
+        TimerFuture::new(Duration::from_secs(2)).await;
+        println!("Spawner: Finish!");
+    });
+
+    // Since the receiver is still alive even when it's done, we `drop' the variable if it exits
+    drop(spawner);
+
+    executor.run()
 }
